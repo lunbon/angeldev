bd5a11ef33b01eb9cc3285f9648cc130d1193227
Oleg Korkeshko <rik.san4ez2018@yandex.ru>
2018-07-18 15:55:13 +0500

    demo

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
 	 SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	 SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/mac/projects/angel_dev/app.db'
+	 SQLALCHEMY_DATABASE_URI = 'sqlite:///~/app.db'
 	 SQLALCHEMY_TRACK_MODIFICATIONS = False
